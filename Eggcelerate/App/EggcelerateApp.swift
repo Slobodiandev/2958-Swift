864e0ca9a91e64b983e1af8fd596fb6e91519327
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-10-29 20:52:43 +0200

    save

---
+++ Eggcelerate/App/EggcelerateApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggcelerateApp.swift
+//  Eggcelerate
+//
+//  Created by Serhii Babchuk on 29.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct EggcelerateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            EALaunchView()
+        }
+    }
+}
